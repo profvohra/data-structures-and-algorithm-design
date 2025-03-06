5cb9c1cc48c07b7ecc5a330620fb3dc7ef23ac24
Rosemina Vohra <rosemina.vohra@njit.edu>
2025-03-06 08:11:00 -0500

    Unit 7"

--- unit07_20Chapter15/SortArrayDriver.java
+++ unit07_20Chapter15/SortArrayDriver.java
@@ -8,11 +8,12 @@ public class SortArrayDriver {
     
     Integer[] numbers = {15, 8, 10, 2, 5};
     SortArray.selectionSort(numbers, numbers.length);
-    System.out.println(Arrays.toString(numbers));
+
     System.out.println();
 
     Integer[] moreNumbers = {2, 5, 8, 3, 9, 4, 1};
     SortArray.insertionSort(moreNumbers, 0, moreNumbers.length - 1);
+
     System.out.println();
 
     Integer[] evenMoreNumbers = {10, 16, 11, 4, 15, 3, 9, 6, 1, 17, 8, 12, 7};
